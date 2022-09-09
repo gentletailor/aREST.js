f16f6c9c47b5c229200fd3eab8b60a683456dc06
Julian Schneider <hi@schneider-julian.de>
2022-09-09 03:39:11 +0200

    rebuild example

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # aREST.js
 
-This fork of aREST.js is a JavaScript library that was made to easily control your projects running aREST without the need of jQuery. It can be used with a simple web browser without Node.js. It works with all the aREST projects using HTTP communication, for example using Arduino with the CC3000 WiFi chip, the Ethernet shield, or the WiFi shield. It also works with the ESP8266 WiFi chip.
+aREST.js is a JavaScript library that was made to easily control your projects running aREST. It can be used with a simple web browser without Node.js. It works with all the aREST projects using HTTP communication, for example using Arduino with the CC3000 WiFi chip, the Ethernet shield, or the WiFi shield. It also works with the ESP8266 WiFi chip.
 
 ## Installation
 
 You can simply embed aREST.js in an HTML page by including the file from the CDN:
 
 ```shell
-https://cdn.rawgit.com/gentletailor/aREST.js/master/aREST.js
+https://cdn.rawgit.com/marcoschwartz/aREST.js/master/aREST.js
 ```
+
+Note that you also need to include jQuery & AjaxQ.
 
 ## Usage
 
@@ -17,7 +19,7 @@ You can simply create a new device in the JavaScript part of your project by giv
 ```js
 var device = new Device(ip_address);
 ```
-Please provide http:// or https:// together with the ip adress, depending on how your Arduino webserver is configured.
+
 ## Getting Started
 
 Let's see a simple example. We are going to create a new aREST device, set pin number 6 to an output, and then turn pin 6 to a HIGH state.
@@ -25,11 +27,13 @@ Let's see a simple example. We are going to create a new aREST device, set pin n
 You first need to include the required libraries in your HTML page:
 
 ```html
-<script type="text/javascript" src="https://cdn.rawgit.com/gentletailor/aREST.js/master/aREST.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="https://cdn.rawgit.com/Foliotek/AjaxQ/master/ajaxq.js"></script>
+<script type="text/javascript" src="https://cdn.rawgit.com/marcoschwartz/aREST.js/master/aREST.js"></script>
 <script type="text/javascript" src="script.js"></script>
 ```
 
-Then, inside the script.js file, use this code to create a new aREST device (modify the IP address with the one from your device, include https or http to specify the connection method), set pin number 6 to an output, and then turn pin 6 to a HIGH state:
+Then, inside the script.js file, use this code to create a new aREST device (modify the IP address with the one from your device), set pin number 6 to an output, and then turn pin 6 to a HIGH state:
 
 ```js
 // Create device
--- examples/script.js
+++ examples/script.js
@@ -1,59 +1,63 @@
-$( document ).ready(function() {
+window.addEventListener(
+  "load",
+  function () {
+    // do something here ...
 
-  // Create new aREST device when button is clicked
-  $('#validate').click(function() {
-    var address = $('#device_address').val();
-    var device = new Device(address);
+    // Create new aREST device when button is clicked
 
-    // Set device pins
-    device.pinMode(6, "OUTPUT");
-    device.pinMode(2, "INPUT");
+    document.querySelector("#validate").addEventListener("click", () => {
+      var address = document.querySelector("#validate").value;
+      var device = new Device(address);
 
-    // Button
-    $('#on').click(function() {
-      device.digitalWrite(6, 1);
-    });
+      // Set device pins
+      device.pinMode(6, "OUTPUT");
+      device.pinMode(2, "INPUT");
 
-    $('#off').click(function() {
-      device.digitalWrite(6, 0);
-    });
-
-    // Analog write
-    $('#slider').mouseup(function() {
-      var val = $('#slider').val();
-      device.analogWrite(6, val);
-    });
-
-    //Analog read every 5 seconds
-    device.analogRead(0, function(data) {
-      $("#A0").html(data.return_value);
-    });
-    setInterval(function() {
-      device.analogRead(0, function(data) {
-        $("#A0").html(data.return_value);
+      // Button
+      document.querySelector("#on").addEventListener("click", () => {
+        device.digitalWrite(6, 1);
       });
-    }, 5000);
 
-    // Digital read every 5 seconds
-    device.digitalRead(11, function(data) {
-      $('#2').html(data.return_value);
-    });  
-    setInterval(function() {
-      device.digitalRead(11, function(data) {
-        $('#2').html(data.return_value);
+      document.querySelector("#off").addEventListener("click", () => {
+        device.digitalWrite(6, 0);
       });
-    }, 5000);
 
-    // Temperature display
-    device.getVariable('temperature', function(data) {
-      $('#temperature').html(data.temperature);
+      // Analog write
+      document.querySelector("#on").addEventListener("mouseup", () => {
+        var val = document.querySelector("#slider").value;
+        device.analogWrite(6, val);
+      });
+
+      //Analog read every 5 seconds
+      device.analogRead(0, function (data) {
+        document.querySelector("#A0").innerHTML = data.return_value;
+      });
+      setInterval(function () {
+        device.analogRead(0, function (data) {
+          document.querySelector("#A0").innerHTML = data.return_value;
+        });
+      }, 5000);
+
+      // Digital read every 5 seconds
+      device.digitalRead(11, function (data) {
+        document.querySelector("#2").innerHTML = data.return_value;
+      });
+      setInterval(function () {
+        device.digitalRead(11, function (data) {
+          document.querySelector("#2").innerHTML = data.return_value;
+        });
+      }, 5000);
+
+      // Temperature display
+      device.getVariable("temperature", function (data) {
+        document.querySelector("#temperature").innerHTML = data.temperature;
+      });
+
+      // Humidity display
+      device.getVariable("humidity", function (data) {
+        document.querySelector("#humidity").innerHTML = data.humidity;
+      });
     });
-
-    // Humidity display
-    device.getVariable('humidity', function(data) {
-      $('#humidity').html(data.humidity);
-    });
-  });
-
-});
-
+  },
+  false
+);
